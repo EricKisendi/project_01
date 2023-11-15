e81a32e7cab129dcd61f328293fe64001071f428
eric kisendi <erickisendi18@gmail.com>
2023-11-15 08:54:13 +0300

    login webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+        <div className="App">
+          <div className = 'header'>
+            <h2 className='head1'>Login</h2>
+            <p className='para1'>Enter your credentials</p>
+          </div>
+          <form className = 'form1'>
+            <input 
+              className='username' 
+              placeholder='Username'
+              type = 'email'
+              spellcheck = 'false'
+              autocomplete = 'off'
+            />
+            <input 
+              className='password' 
+              placeholder='Password'
+              id = 'password'
+              type='password'
+              spellcheck = 'false'
+            />
+            <button>LOGIN</button>
+          </form>
+        </div>
     </div>
   );
 }
